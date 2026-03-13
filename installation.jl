a2085cbef18b8d5fc0cff8151d57b84cd9058bd9
Katie Dixon <kpd19@case.edu>
2026-03-13 15:36:05 -0500

    Adding julia package info

---
+++ installation.jl
@@ -0,0 +1,25 @@
+using Pkg
+
+Pkg.add("DifferentialEquations")
+Pkg.add("CSV")
+Pkg.add("DataFrames")
+Pkg.add("Distributions")
+Pkg.add("LinearAlgebra")
+Pkg.add("Tables")
+Pkg.add("Plots")
+Pkg.add("LaTeXStrings")
+Pkg.add("Random")
+Pkg.add("LinearAlgebra")
+Pkg.add("FFTW")
+Pkg.add("SparseArrays")
+Pkg.add("BlockDiagonals")
+Pkg.add("Symbolics")
+Pkg.add("DSP")          # convolve
+Pkg.add("Statistics")   # mean
+Pkg.add("SpecialFunctions")  # gamma, besselj
+Pkg.add("Combinatorics")     # binomial (comb)
+Pkg.add("Polynomials")
+Pkg.add("HypergeometricFunctions")
+Pkg.add("ModelingToolkit")
+Pkg.add("Roots")
+Pkg.add("Interpolations")
